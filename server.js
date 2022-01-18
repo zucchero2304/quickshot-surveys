460edd3555578b97bf91a76a49e1b64f6b989e95
zucchero2304 <remek.piwowarski@gmail.com>
2022-01-18 18:16:39 +0100

    Fix

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.use(require("./routes/surveyCodes"))
 // get driver connection
 const dbo = require("./db/connection")
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+}
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './client/build/index.html'));
 })
